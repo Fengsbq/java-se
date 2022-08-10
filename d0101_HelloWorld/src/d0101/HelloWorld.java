0ef1230bfe11933045e5eb1ef5928f121a6e56a7
Fu Xiantao <18856161393@163.com>
2022-08-10 22:00:02 +0800

    初始提交

---
+++ d0101_HelloWorld/src/d0101/HelloWorld.java
@@ -0,0 +1,14 @@
+package d0101;
+public class HelloWorld {
+	//主方法、入口方法
+	public static void main(String[] args) {
+		System.out.println("Hello world!!!");
+		System.out.println("师姐你好！！！");    
+	}
+}
+
+
+
+
+
+
---
+++ d1304_序列化/src/d1304/Test1.java
@@ -0,0 +1,29 @@
+package d1304;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class Test1 {
+	public static void main(String[] args) throws Exception {
+		Student s = 
+		 new Student(9527,"唐伯虎","男",19);
+		/*
+		 * OOS--FOS--f3
+		 */
+		ObjectOutputStream out = 
+		 new ObjectOutputStream(
+		 new FileOutputStream("d:/abc/f3"));
+		
+		out.writeObject(s);
+		
+		out.close();
+	}
+}
+
+
+
+
+
+
+
+
